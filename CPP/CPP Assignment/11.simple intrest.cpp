4751b077f3f7f884763ac6262109ecc04c60b860
jayjoshi1912 <109573770+jayjoshi1912@users.noreply.github.com>
2022-07-23 10:06:10 +0530

    CPP

--- CPP/CPP Assignment/11.simple intrest.cpp
+++ CPP/CPP Assignment/11.simple intrest.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 using namespace std;
 
-class SIMPLE INTREST
+class SI
 {
 	public:
 		int pa;
-		float rate,year;
-		SIMP
+		float rate , year;
+		SI()
+		{
+			rate = 2.5;
+		}
+	   void si()
+		{
+			cout<<"================================="<<endl;
+			cout<<"          SIMPLE INTREST "<<endl<<endl;
+			cout<<"================================="<<endl<<endl;
+			
+			cout<<"Principal Amount        : ";
+			cin>>pa;
+			cout<<endl<<endl;
+			cout<<"Year                    : ";
+			cin>>year;
+			cout<<endl<<endl;
+			cout<<"Rate                    : "<<rate<<endl<<endl<<endl;
+			cout<<"Simple Intrest Is       : "<<(pa*year*rate)/(float)100;
+			
+		}
 };
 int main()
 {
+	SI s;
+	s.si();
 	return 0;
 }
